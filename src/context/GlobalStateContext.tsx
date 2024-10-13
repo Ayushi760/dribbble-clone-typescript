58af327eb282ca58380120e6026bdb60be85e3f4
Ayushi Saxena <ayushisaxena@presidio.com>
2024-10-13 22:44:31 +0530

    first commit

---
+++ src/context/GlobalStateContext.tsx
@@ -0,0 +1,71 @@
+import { createContext, useEffect, useReducer } from "react";
+
+export const GlobalStateContext = createContext();
+
+const initialState = {
+  users: [],
+  currentUser: JSON.parse(localStorage.getItem("currentUser")) || null,
+  isAuthenticated: JSON.parse(localStorage.getItem("currentUser"))
+    ? true
+    : false,
+  loading: false,
+  error: null,
+  categories: []
+};
+
+const globalReducer = (state, action) => {
+  switch (action.type) {
+    case "REGISTER":
+      return {
+        ...state,
+        users: [...state.users, action.payload],
+      };
+    case "LOGIN":
+      return {
+        ...state,
+        isAuthenticated: true,
+        currentUser: action.payload,
+      };
+    case "LOGOUT":
+      return {
+        ...state,
+        isAuthenticated: false,
+        currentUser: null,
+      };
+    case "SET_LOADING":
+      return {
+        ...state,
+        loading: action.payload,
+      };
+    case "SET_ERROR":
+      return {
+        ...state,
+        error: action.payload,
+      };
+    case "SET_CATEGORIES": 
+      return { 
+        ...state, 
+        categories: action.payload 
+      };
+    default:
+      return state;
+  }
+};
+
+export const GlobalStateProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(globalReducer, initialState);
+
+  useEffect(() => {
+    if (state.currentUser) {
+      localStorage.setItem("currentUser", JSON.stringify(state.currentUser));
+    } else {
+      localStorage.removeItem("currentUser");
+    }
+  }, [state.currentUser]);
+
+  return (
+    <GlobalStateContext.Provider value={{ state, dispatch }}>
+      {children}
+    </GlobalStateContext.Provider>
+  );
+};
---
+++ src/pages/Home.tsx
@@ -0,0 +1,35 @@
+import ArtistsSlider from "../components/ArtistsSlider";
+import ExploreDesigns from "../components/ExploreDesigns";
+import { GlobalStateContext } from "../context/GlobalStateContext";
+import CallToAction from "../components/CallToAction";
+import CategorySlider from "../components/CategorySlider";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+
+const Home = () => {
+  const { state } = useContext(GlobalStateContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (state.isAuthenticated) {
+      navigate("/dashboard");
+    }
+  }, [state.isAuthenticated, navigate]);
+
+  return (
+    <div className="bg-[#f8f7f4] flex flex-col items-center w-full">
+      <Header />
+      <Hero />
+      <ArtistsSlider />
+      <ExploreDesigns />
+      <CallToAction />
+      <CategorySlider />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
